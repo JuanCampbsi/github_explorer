b0f2989ee952885cd7914334eb8857c20df22b46
Juan Campos <juancampos.bsi@outlook.com>
2021-08-17 15:12:38 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export function App(){
-    return <h1>Hello Word</h1>
+    return <h1>Hello Trail ReactJs</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode:'development',
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -9,7 +10,18 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js', '.jsx'],
+    },    
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+    
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
     },
+    
+    
     module: {
         rules: [
             {
